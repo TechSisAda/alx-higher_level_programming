b952a1fed272d1c9bc7bb1d885328deba18e244b
Adaeze Nnonyelu <nnonyeluadaeze@gmail.com>
2023-06-19 20:12:11 +0100

    second commit

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,14 +1,17 @@
 #!/usr/bin/node
-
 const request = require('request');
 const fs = require('fs');
 const url = process.argv[2];
-const filePath = process.argv[3];
+const file = process.argv[3];
 
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
   } else {
-    fs.writeFile(filePath, body, 'utf-8');
+    fs.writeFile(file, body, 'utf8', function (error, data) {
+      if (error) {
+        console.error(error);
+      }
+    });
   }
 });
